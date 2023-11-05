967ec6ff374fee2c06a69c99fe673039c6d83415
kevschuetz <kevin.hotz@gmx.at>
2023-11-05 09:47:50 +0100

    manager setup

--- volumes/postgres/init/init.sql
+++ volumes/postgres/init/init.sql
@@ -1,10 +1,10 @@
 --
 -- Roles
 --
-
 CREATE ROLE registry;
 ALTER ROLE registry WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'registry';
-
+CREATE ROLE manager;
+ALTER ROLE manager WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'manager';
 
 --
 -- Database "service_registry"
@@ -20,11 +20,18 @@ ALTER DATABASE service_registry OWNER TO registry;
 
 GRANT USAGE ON SCHEMA public TO registry;
 GRANT CREATE ON SCHEMA public TO registry;
-
--- Tables of service registry
-
-CREATE TABLE public.test (
-    id integer primary key
-);
-
 -- Service registry initialization complete
+--
+-- Database "manager"
+--
+--
+-- Name: manager; Type: DATABASE; Schema: -; Owner: manager
+--
+CREATE DATABASE manager WITH TEMPLATE = template0 ENCODING = 'UTF8';
+ALTER DATABASE manager OWNER TO manager;
+
+\connect manager
+
+GRANT USAGE ON SCHEMA public TO manager;
+GRANT CREATE ON SCHEMA public TO manager;
+-- Manager DB initialization complete
